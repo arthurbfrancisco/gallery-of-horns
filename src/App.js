5309730008eb4ee00101a8c26a74c4b073e38b9b
Arthur Franciso <arthur.b.francisco@gmail.com>
2023-05-12 01:40:42 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
-import React from 'react';
-import Header from './component/Header';
-import Footer from './component/Footer';
-import Main from './component/Main';
+import React from "react";
+import Header from "./Header.js";
+import Main from "./Main.js";
+import Footer from "./Footer.js";
+import './App.css';
+import data from './data.json';
+import ClickedBeast from './ClickedBeast.js'
+
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hearts: '',
+      isModalDisplaying: false,
+      beastName: 'test',
+      clickedBeast: {}
+    }
+  }
+  addHearts = () => {
+    this.setState({
+      hearts: this.state.hearts + <span role="img" aria-label="blackHeart">'🖤'</span>
+    });
+  }
+  handleShowModal = (clickedBeast) => {
+    this.setState({
+      isModalDisplaying: true,
+      clickedBeast: clickedBeast
+    });
+    console.log('modal was clicked');
+
+  }
+
+  handleCloseModal = () => {
+    this.setState({
+      isModalDisplaying: false
+    })
+  }
+
   render() {
     return (
-      <div>
-        <Header />
-        <Main />
+      <>
+        <Header
+          hearts={this.state.hearts}
+        />
+        <Main
+          addHearts={this.addHearts}
+          data={data}
+          handleShowModal={this.handleShowModal}
+        />
         <Footer />
-      </div>
-    );
+        <ClickedBeast
+        show={this.state.isModalDisplaying}
+        onHide={this.handleCloseModal}
+        clickedBeast={this.state.clickedBeast}
+        />
+      </>
+    )
   }
-}
+};
+
 export default App;
 
 
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,27 +1,41 @@
-import React from 'react';
-import HornedBeast from './Hornedbeast';
-import data from'../data.json';
+import React from "react";
+import HornedBeast from './HornedBeast';
+// import HornedOneData from './data.json';
 import Col from 'react-bootstrap/Col';
-
+import Row from 'react-bootstrap/Row';
 
 class Main extends React.Component {
   render() {
-    console.log(data);
-    return (
- 
-        <Col>
-        {data.map((beast, index) => (
-          <HornedBeast
-            key={index}
-            title={beast.title}
-            imageUrl={beast.image_url}
-            description={beast.description}
+
+    let hornedOne = this.props.data.map((horn, idx) => {
+        //     HornedOneData.forEach((horn, idx) => {
+        // hornedOne.push(
+        //   <Col md={4} key={idx}>
+
+      return (<Col key={idx} className="mt-4">
+        
+        <HornedBeast
+          imageURL={horn.image_url}
+          title={horn.title}
+          alt={horn.alt}
+          description={horn.description}
+          key={horn.keyword}
+          horns={horn.horns}
+          addHearts={this.props.addHearts}
+          handleShowModal={this.props.handleShowModal}
+          beast={horn}
           />
-        ))}
-     </Col>
+          </Col>)
+      });
+
+    return (
+      <main>
+        <Row xs={1} sm={2} md={3} lg={4}>
+        {hornedOne}
+        </Row>
+      </main>
     );
   }
 }
-
 
 export default Main;
